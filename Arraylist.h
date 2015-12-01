8b4481983f46a3bda88da9a5570c8982c006a33c
Gejo <engineergoodz@gmail.com>
2015-12-01 11:45:53 +0530

    Create Arraylist.h

---
+++ Arraylist.h
@@ -0,0 +1,14 @@
+#ifndef _ARRAYLIST_H
+#define _ARRAYLIST_H
+
+typedef struct _arraylist ArrayList;
+
+extern ArrayList *arraylist_create();
+extern void arraylist_set_data(ArrayList *, void **);
+extern void arraylist_add(ArrayList *, void *);
+extern void *arraylist_get(ArrayList *, int);
+extern void arraylist_remove(ArrayList *, int);
+extern void arraylist_clear(ArrayList *);
+extern void arraylist_deallocate(ArrayList *);
+
+#endif /* _ARRAYLIST_H */
